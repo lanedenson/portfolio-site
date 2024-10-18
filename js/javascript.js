eec58a31845522e1732613d91aa87c2126c86bb5
Lane Denson <lanedenson@gmail.com>
2024-10-18 16:31:41 -0500

    Refactored index.html. Enhanced CSS for better readability and responsiveness. Updated JavaScript for header behavior on scroll.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,10 @@
-function output(value) {
-    const para = document.createElement('p');
-    document.body.appendChild(para);
-    para.textContent = `{value}`;
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        const header = document.querySelector('.sticky-header');
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.classList.add('shrink');
+        } else {
+            header.classList.remove('shrink');
+        }
+    };
+});
